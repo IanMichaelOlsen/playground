1f1b743b5da8421fa62c42b06ee41873ec53ae14
Ian Michael Olsen <iolsenhs@gmail.com>
2023-01-17 04:31:01 -0600

    Testing new menu options and network connectivity

--- main.cpp
+++ main.cpp
@@ -1,21 +1,200 @@
 #include "main.h"
 
+#include <limits>
+#include <cstdlib>
+
+// Function to display menu options
+void displayMenu() {
+    std::cout << "Please select an option:" << std::endl;
+    std::cout << "1. Create a new key" << std::endl;
+    std::cout << "2. Use an existing key" << std::endl;
+    std::cout << "3. Create a new message" << std::endl;
+    std::cout << "4. Use an existing message" << std::endl;
+    std::cout << "5. Encrypt the message" << std::endl;
+    std::cout << "6. Decrypt the message" << std::endl;
+    std::cout << "7. Exit the program" << std::endl;
+    std::cout << "8. Ping Google.com" << std::endl;
+}
+
+// Function to get user input and validate it
+int getInput() {
+    int choice;
+    std::cin >> choice;
+    while (std::cin.fail() || choice < 1 || choice > 8) {
+        std::cout << "Invalid input. Please enter a number between 1 and 8." << std::endl;
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        displayMenu();
+        std::cout << "Please select a option: ";
+        std::cin >> choice;
+    }
+    return choice;
+}
+
 int main() {
+    std::string key, message, userMessageFile, userKeyFile;
+    std::string newRandomKey;
+    int choice;
 
-    unsigned __int64 d = generateRandomNumber();
-    //Print out the Decryption Key
-    std::cout << "Exponent: " << d << "\n";
+    while (true) {
+        displayMenu();
+        // Ask user for input
+        std::cout << "Enter your choice: ";
+        choice = getInput();
+        std::ofstream keyFile;
+        std::string newMessage;
+        //Create the encrypted and decrypted variables
+        std::string encryptedMessage;
+        std::string decryptedMessage;
+        std::ifstream messageFile;
+        std::string messageString;
+        std::string fileName;
+        std::string fileNameNewMessage;
+        std::ofstream file(fileName);
+        unsigned __int64 d = generateRandomNumber();
 
-    //Write the key to a file, ask user what to name the decrypt file
-    std::string fileName;
-    std::cout << "Enter the name of the file to save the key to: ";
-    std::cin >> fileName;
-    std::ofstream file(fileName);
-    file << d;
-    file.close();
+        switch (choice) {
+            case 1:
+                std::cout << "Creating a new key..." << std::endl;
+                // code to create new key
+                //Print out the Decryption Key
+                std::cout << "Key: " << d << "\n";
+                //Write the key to a file, ask user what to name the decrypt file
+                std::cout << "Enter the name of the file to save the key to: ";
+                std::cin >> fileName;
+                file.open(fileName);
+                file << d;
+                file.close();
+                //Inform the user that the key has been saved
+                std::cout << "Key saved to " << fileName << "\n";
+                break;
+            case 2:
+                std::cout << "Using an existing key..." << std::endl;
+                // code to use existing key
+                break;
+            case 3:
+                std::cout << "Creating a new message..." << std::endl;
+                //create a new message and save it to a file
+                //Enter a message to encrypt
+                std::cout << "Enter a message to encrypt: ";
+                std::cin >> newMessage;
+                //Save message to file name given by user
+                std::cout << "Enter a name for the message file: ";
+                std::cin >> fileNameNewMessage;
+                keyFile.open(fileNameNewMessage);
+                keyFile << newMessage;
+                keyFile.close();
+                //Inform user that message has been saved
+                std::cout << "Message saved to file: " << fileNameNewMessage << std::endl;
+                std::cout << "MAKE SURE TO ENCRYPT THE MESSAGE WITH A KEY BEFORE SENDING IT TO ANYONE!" << std::endl;
+                break;
+            case 4:
+                std::cout << "Using an existing message..." << std::endl;
+                // code to use existing message
+                break;
+            case 5:
+                std::cout << "Encrypting the message..." << std::endl;
+                //Get a message file name from the user
+                std::cout << "Enter the name of the message file: ";
+                std::cin >> userMessageFile;
+                //open the message file and save it to a string
+                messageFile.open(userMessageFile);
+                messageFile >> messageString;
+                messageFile.close();
+                //Show the user the message that will be encrypted
+                std::cout << "Message to encrypt: " << messageString << std::endl;
+                //Get the key file name from the user
+                std::cout << "Enter the name of the key file: ";
+                std::cin >> userKeyFile;
+                //Open the key file and save it to a string
+                messageFile.open(userKeyFile);
+                messageFile >> key;
+                messageFile.close();
+                //Show the user the key that will be used to encrypt the message
+                std::cout << "Key to encrypt with: " << key << std::endl;
+                //Encrypt the message with the new key using a julius caesar cipher
+                for (int i = 0; i < messageString.length(); i++) {
+                    encryptedMessage += (char) (messageString[i] - std::stoi(key));
+                }
+                //Show the user the encrypted message
+                std::cout << "Encrypted message: " << encryptedMessage << std::endl;
+                //Save the encrypted message to a file
+                std::cout << "Enter a name for the encrypted message file: ";
+                std::cin >> fileName;
+                keyFile.open(fileName);
+                keyFile << encryptedMessage;
+                keyFile.close();
+                //Inform user that message has been saved
+                std::cout << "Encrypted message saved to file: " << fileName << std::endl;
 
-    //Inform user that the key has been written to a file
-    std::cout << "Decryption Key has been written to " << fileName << std::endl;
+                break;
+            case 6:
+                std::cout << "Decrypting the message..." << std::endl;
+                // code to decrypt the message
+                break;
+            case 7:
+                std::cout << "Exiting the program..." << std::endl;
+                return 0;
+            case 8:
+                std::cout << "Pinging Google.com 50 times...." << std::endl;
+                //Ping google.com 50 times
+                for (int i = 0; i < 5; i++) {
+                    std::string IP ("8.8.8.8");
+                    std::string command = "ping -n 10 " + IP;
+                    std::system(command.c_str());
+                }
+                break;
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+    switch (selection) {
+        case 1:
+            std::cout << "You selected to create a new key." << std::endl;
+            //Create a new key
+            std::cout << "Your new key is: " << d << std::endl;
+
+            break;
+        case 2:
+            std::cout << "You selected to use an existing key." << std::endl;
+            //Use an existing key
+
+            break;
+        case 3:
+            std::cout << "You selected to create a new message." << std::endl;
+            break;
+        case 4:
+            std::cout << "You selected to use an existing message." << std::endl;
+            break;
+        case 5:
+            std::cout << "You selected to encrypt the message." << std::endl;
+            break;
+        case 6:
+            std::cout << "You selected to decrypt the message." << std::endl;
+            break;
+        case 7:
+            std::cout << "You selected to exit the program." << std::endl;
+            break;
+        default:
+            std::cout << "You did not enter a valid selection." << std::endl;
+            break;
+    }
+
+
     //Pause the program
     std::cin.get();
     //Enter a message to encrypt
@@ -83,4 +262,4 @@ int main() {
     }
 
     return 0;
-}
+} */
