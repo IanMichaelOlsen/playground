1d603da8f8e4df0c7986729ab805bc0bdb8b3617
Ian Michael Olsen <iolsenhs@gmail.com>
2023-01-17 04:28:16 -0600

    Changed out maximum integer.

--- fermat.cpp
+++ fermat.cpp
@@ -1,14 +1,8 @@
-
 #include "fermat.h"
 
 unsigned __int64 generateRandomNumber() {
     unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
     std::mt19937_64 generator(seed);
     //Generate random number
-    return generator();
+    return generator() % 1000000000;
 }
-
-
-
-
-
